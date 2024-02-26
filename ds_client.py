8d615574e22392bb8db8aba718b7da277dc82940
Nicole Kwan <nicolekvan@gmail.com>
2024-02-25 21:28:55 -0800

    connect server function

--- ds_client.py
+++ ds_client.py
@@ -11,4 +11,33 @@ import time
 from ds_protocol import extract_json
 
 def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
+    pass
+
+def join_server(server, port, username, password):
+    data = {
+        "join": {
+            "username": username,
+            "password": password,
+            "token": ""
+        }
+    }
+
+    # formats as json string
+    json_message = json.dumps(data)
+
+    with socket.socker(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((server, port))
+
+        send = client.makefile('w')
+        recv = client.makefile('r')
+
+        send.write(json_message + '\r\n')
+        send.flush()
+
+        response = recv.readline()
+        get_data = extract_json(response)
+
+        print(get_data.message)
+
+    return get_data
     
